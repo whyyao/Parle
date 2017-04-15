77a89d45bf42d6b2576d9b699bf3a3c00bd94566
Yuan Yao <yuanyao.us@gmail.com>
2017-04-15 14:49:58 -0700

    added logo icon

---
+++ app/src/main/java/cs48/project/com/parl/Conversation.java
@@ -0,0 +1,40 @@
+package cs48.project.com.parl;
+
+/**
+ * Created by yaoyuan on 4/15/17.
+ */
+
+public class Conversation {
+    private String name;
+    private String lastMessage;
+    private String time;
+
+    public Conversation(String name, String lastMessage, String time){
+        this.name = name;
+        this.lastMessage = lastMessage;
+        this.time = time;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getLastMessage(){
+        return lastMessage;
+    }
+    public void setLastMessage(String lastMessage){
+        this.lastMessage = lastMessage;
+    }
+
+    public String getTime(){
+        return time;
+    }
+    public void setTime(String time){
+        this.time = time;
+    }
+
+}
--- app/src/main/java/cs48/project/com/parl/MainActivity.java
+++ app/src/main/java/cs48/project/com/parl/MainActivity.java
@@ -2,6 +2,9 @@ package cs48.project.com.parl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Conversation> conversations = new ArrayList<>();
+        conversations.add(new Conversation("sam", "first message", "time"));
+        conversations.add(new Conversation("michael", "second message", "time"));
+        conversations.add(new Conversation("yao", "third message", "time"));
+        conversations.add(new Conversation("chandler", "fourth message", "time"));
+        conversations.add(new Conversation("jake", "fifth message", "time"));
+        conversations.add(new Conversation("Dan", "fifth message", "time"));
+
+        ListView conversationListView = (ListView) findViewById(R.id.list);
+        final ConversationAdapter adapter = new ConversationAdapter(this, conversations);
+        conversationListView.setAdapter(adapter);
+
     }
 }
