d181786d35c758794607bc4cbd620d1cd0cc22f7
Yuan Yao <yuanyao.us@gmail.com>
2017-05-06 21:29:56 -0700

    Added logout botton in me tab

--- app/src/main/java/cs48/project/com/parl/ui/activities/ConvoListingActivity.java
+++ app/src/main/java/cs48/project/com/parl/ui/activities/ConvoListingActivity.java
@@ -1,20 +1,14 @@
 package cs48.project.com.parl.ui.activities;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import cs48.project.com.parl.R;
-import cs48.project.com.parl.core.logout.LogoutContract;
 import cs48.project.com.parl.core.logout.LogoutPresenter;
 import cs48.project.com.parl.ui.adapters.ConvoListingPagerAdapter;
 import cs48.project.com.parl.ui.fragments.ConvoFragment;
@@ -67,7 +61,7 @@ public class ConvoListingActivity extends AppCompatActivity {
         mTabLayoutUserListing.setupWithViewPager(mViewPagerUserListing);
 
         setupTabIcons();
-        mLogoutPresenter = new LogoutPresenter(this);
+//        mLogoutPresenter = new LogoutPresenter(this);
     }
 
     private void setupTabIcons() {
