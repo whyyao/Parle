ae3b29b48834f98562e0ea9b9511ae6829035b5c
jakebliss <zakarybliss@umail.ucsb.edu>
2017-05-08 17:49:00 -0700

    Working on Add Contact Activity

---
+++ app/src/main/java/cs48/project/com/parl/ui/activities/ContactAddActivity.java
@@ -0,0 +1,10 @@
+package cs48.project.com.parl.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jakebliss on 5/8/17.
+ */
+
+public class ContactAddActivity extends AppCompatActivity{
+}
